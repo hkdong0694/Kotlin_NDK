ae02e58c8be93d1e2aef492fe359abee3ebab64c
한경동 <hkd0694@gmail.com>
2021-08-02 23:43:32 +0900

    NDK 정리

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,11 @@
 #include <jni.h>
 #include <string>
 
+// jstring -> JNI 에서 사용하는 데이터 구조, 자바 문자열의 포인터를 가리키는 포인터 타입이다.
 extern "C" JNIEXPORT jstring JNICALL
+// 함수 위치의 절대 경로 com_example_ndksample -> 패키지 명
+// JNIEnv* -> Virtual Machine을 가리키는 포인터
+// jobject -> 자바에서 this 객체를 가리키는 포인터
 Java_com_example_ndksample_MainActivity_stringFromJNI(JNIEnv* env,jobject ) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
--- app/src/main/java/com/example/ndksample/MainActivity.kt
+++ app/src/main/java/com/example/ndksample/MainActivity.kt
@@ -6,10 +6,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // 라이브러리 내부에 다음과 같은 함수명이 있다는 것을 JVM 에게 알려준다.
     external fun stringFromJNI() : String
 
     companion object {
         init {
+            // 아래 함수를 이용해 C++ 빌드 파일을 라이브러리로 변환해 플랫폼에 적재, 내부의 네이티브 함수를 사용할 수 있도록 한다.
+            // native-lib 라는 라이브러리를 load 하면 파일명은 lib + native-lib + .so
+            // libnative-lib.so 가 된다.
             System.loadLibrary("native-lib")
         }
     }
