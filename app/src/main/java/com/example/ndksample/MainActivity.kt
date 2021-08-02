db9ea7d4c32d9efc95cd66d33ad96d308f075c10
한경동 <hkd0694@gmail.com>
2021-08-02 22:45:12 +0900

    NDK 세팅

--- app/src/main/java/com/example/ndksample/MainActivity.kt
+++ app/src/main/java/com/example/ndksample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.ndksample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    external fun stringFromJNI() : String
+
+    companion object {
+        init {
+            System.loadLibrary("jniExample")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_main.text = stringFromJNI()
     }
 }
